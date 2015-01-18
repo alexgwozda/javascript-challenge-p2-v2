526ccafe4f7a93ef87431669cada0b93513762f2
Alex Gwozda <gwozda@gmail.com>
2015-01-17 20:15:46 -0800

    Completed documentation
    Todo: test all files

--- astronauts2.js
+++ astronauts2.js
@@ -21,28 +21,29 @@ var names = [
  * Split surName off from givenNames (first and any middle).  
  * 
  * @param {array} names
+ * @returns {array} orderedNames - alphebetized array by last name.  
  */
 function alphabetizer(names) {
   var orderedNames = [];
   var splitNamesLength;
-  var lastIndex;
+  var lastNameIndex;
   var givenNames = [];
   var surName = [];
 
   for (var i = 0; i < names.length; i++) {
-
     splitNamesLength = names[i].split(" ").length;
-    lastIndex = splitNamesLength - 1;
-    givenNames = splitName(i, 0);
+    lastNameIndex = splitNamesLength - 1;
 
-    /** If there are any middle names, also concatenate all but the last surname to givenNames. */
+    /** Variable givenNames only grabs the first name at first */
+    givenNames = splitName(i, 0);
+    /** Then if there are any middle names, also concatenate all but the last surname to givenNames. */
     if (names[i].length > 2) {
-        for (var j = 1; j < lastIndex; j++) {
+        for (var j = 1; j < lastINamendex; j++) {
             givenNames[i] += " " + splitName(i, j);
         }
     }
 
-    surName[i] = splitName(i, lastIndex);
+    surName[i] = splitName(i, lastNameIndex);
     orderedNames[i] = [surName, givenNames];
     orderedNames[i] = orderedNames[i].join(', ');
   }
@@ -55,9 +56,9 @@ function alphabetizer(names) {
 /** 
  * Helper function to automate splitting.
  * 
- * @param personIndex
- * @param nameIndex - index of the desired piece of name for the person.
- * @returns {string} selectedName - the desired name piece requested by the function call
+ * @param personIndex - the person's name to split
+ * @param nameIndex - index of the desired part of name for the person.
+ * @returns {string} selectedName - the desired part of name requested by the function call
  */
 function splitName(personIndex, nameIndex) {
   var selectedName = names[personIndex].split(' ')[nameIndex];
